6837309a1696b4ab28ac5294848021a82af64ba0
caioedlobo <caioeduardolobo@gmail.com>
2025-04-11 19:10:01 -0300

    add logger middleware

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func (app *application) serve() error {
 	srv := fiber.New()
 
+	srv.Use(logger.New())
+
 	srv.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.SendString("Hello World")
 	})
+
+	//srv.Use(middleware.Logger())
+	//router.SetupRoutes(srv)
 
 	err := srv.Listen(":3000")
 	if err != nil {
